c0106f72aef427b8102286940d2b70b361066544
GZ-Metal-Cell <1904817346@qq.com>
2024-01-12 12:34:16 +0800

    Site updated: 2024-01-12 12:34:07

--- js/fancybox-images.js
+++ js/fancybox-images.js
@@ -4,8 +4,8 @@ function initFancybox() {
         if ($(this).hasClass('nofancybox')) return;
         var alt = this.alt;
         $(this).wrap(
-            '<a href="' + ($(this).attr('data-src') == null ? this.src :
-                $(this).attr('data-src')) + '" title="' + alt + '" data-src="' + this.src + '" class="fancybox" data-fancybox="fancybox-gallery-img"></a>');
+            '<a href="' + ($(this).attr('data-original') == null ? this.src :
+                $(this).attr('data-original')) + '" title="' + alt + '" data-original="' + this.src + '" class="fancybox" data-fancybox="fancybox-gallery-img"></a>');
     });
     $(this).find('.fancybox').each(function () {
         $(this).attr('rel', 'article');
