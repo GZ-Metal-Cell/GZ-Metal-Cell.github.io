68e85c662a2248553fb7c877306d60e9ddd30ef2
GZ-Metal-Cell <1904817346@qq.com>
2024-02-06 12:48:15 +0800

    Site updated: 2024-02-06 12:48:09

--- 2024/02/04/Web-ThreeJS/pages/01_12/src/World/components/Train/Train.js
+++ 2024/02/04/Web-ThreeJS/pages/01_12/src/World/components/Train/Train.js
@@ -5,28 +5,28 @@ import { createMeshes } from './meshes.js';
 const wheelSpeed = MathUtils.degToRad(24);
 
 class Train extends Group {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.meshes = createMeshes();
+        this.meshes = createMeshes();
 
-    this.add(
-      this.meshes.nose,
-      this.meshes.cabin,
-      this.meshes.chimney,
-      this.meshes.smallWheelRear,
-      this.meshes.smallWheelCenter,
-      this.meshes.smallWheelFront,
-      this.meshes.bigWheel,
-    );
-  }
+        this.add(
+            this.meshes.nose,
+            this.meshes.cabin,
+            this.meshes.chimney,
+            this.meshes.smallWheelRear,
+            this.meshes.smallWheelCenter,
+            this.meshes.smallWheelFront,
+            this.meshes.bigWheel,
+        );
+    }
 
-  tick(delta) {
-    this.meshes.bigWheel.rotation.y += wheelSpeed * delta;
-    this.meshes.smallWheelRear.rotation.y += wheelSpeed * delta;
-    this.meshes.smallWheelCenter.rotation.y += wheelSpeed * delta;
-    this.meshes.smallWheelFront.rotation.y += wheelSpeed * delta;
-  }
+    tick(delta) {
+        this.meshes.bigWheel.rotation.y += wheelSpeed * delta;
+        this.meshes.smallWheelRear.rotation.y += wheelSpeed * delta;
+        this.meshes.smallWheelCenter.rotation.y += wheelSpeed * delta;
+        this.meshes.smallWheelFront.rotation.y += wheelSpeed * delta;
+    }
 }
 
 export { Train };
