822a07c4f7ad149c0a1f4265abac4a8d62e875c2
GZ-Metal-Cell <1904817346@qq.com>
2024-02-27 22:07:09 +0800

    Site updated: 2024-02-27 22:07:02

--- js/widget/fancybox.js
+++ js/widget/fancybox.js
@@ -1,43 +1,46 @@
 function initFancybox() {
-    $('article img').each(function () {
-        if ($(this).parent().hasClass('fancybox')) return;
-        if ($(this).hasClass('no-fancybox')) return;
-        var alt = this.alt;
-        $(this).wrap(
-            '<a ' +
-                'class="fancybox"' +
-                'href="' + ($(this).attr('data-original') == null ? this.src : $(this).attr('data-original')) +
-                '"data-caption="' + alt + '"' +
-                'data-fancybox="gallery"' +
-                'data-thumb='+ ($(this).attr('data-original') == null ? this.src :$(this).attr('data-original')) + '>' +
-            '</a>');
-    });
-    Fancybox.bind('[data-fancybox]', {
-        Hash: false,
-        Images: {
-          Panzoom: {
-            maxScale: 4
-          }
-        },
-        Carousel: {
-          transition: 'slide'
-        },
-        Toolbar: {
-          display: {
-            left: ['infobar'],
-            middle: [
-              'zoomIn',
-              'zoomOut',
-              'toggle1to1',
-              'rotateCCW',
-              'rotateCW',
-            ],
-            right: ['slideshow', 'thumbs', 'close']
-          }
-        }
-    })
+	$('article img').each(function () {
+		if ($(this).parent().hasClass('fancybox')) return;
+		if ($(this).hasClass('no-fancybox')) return;
+		var alt = this.alt;
+		$(this).wrap(
+			'<a ' +
+			'class="fancybox"' +
+			'href="' + ($(this).attr('data-original') == null ? this.src : $(this).attr('data-original')) +
+			'"data-caption="' + alt + '"' +
+			'data-fancybox="gallery"' +
+			'data-thumb=' + ($(this).attr('data-original') == null ? this.src : $(this).attr('data-original')) + '>' +
+			'</a>');
+	});
+	Fancybox.bind('[data-fancybox]', {
+		Thumbs: {
+			showOnStart: false,
+		},
+		Hash: false,
+		Images: {
+			Panzoom: {
+				maxScale: 4
+			}
+		},
+		Carousel: {
+			transition: 'slide'
+		},
+		Toolbar: {
+			display: {
+				left: ['infobar'],
+				middle: [
+					'zoomIn',
+					'zoomOut',
+					'toggle1to1',
+					'rotateCCW',
+					'rotateCW',
+				],
+				right: ['slideshow', 'thumbs', 'close']
+			}
+		}
+	})
 }
 
-$(document).ready(function() {
-    initFancybox();
+$(document).ready(function () {
+	initFancybox();
 });
