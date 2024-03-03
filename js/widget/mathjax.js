7acd193dec44013673fcdfdf775ca61cb4caf592
GZ-Metal-Cell <1904817346@qq.com>
2024-03-03 08:55:16 +0800

    Site updated: 2024-03-03 08:55:05

--- js/widget/mathjax.js
+++ js/widget/mathjax.js
@@ -1,12 +1,9 @@
 function initMathjax() {
-    // 创建并添加第一个 <script> 元素
     $('head').append($('<script>').html("MathJax = {tex: {inlineMath: [['$', '$'], ['\\\\(', '\\\\)']]}};"));
-
-    // 创建并添加第二个 <script> 元素
     var $scriptElement2 = $('<script>', {
         id: 'MathJax-script',
         async: true,
-        src: mathjax_url // 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js'
+        src: mathjax_url
     });
     $('head').append($scriptElement2);
 }
