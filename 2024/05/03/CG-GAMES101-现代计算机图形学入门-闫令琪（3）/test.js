70f01ebac5a10f1999234314ac9ee9fa474de595
GZ-Metal-Cell <1904817346@qq.com>
2024-05-04 18:27:05 +0800

    Site updated: 2024-05-04 18:26:53

--- 2024/05/03/CG-GAMES101-现代计算机图形学入门-闫令琪（3）/test.js
+++
@@ -1,6 +0,0 @@
-// $(document).ready(function () {
-//     $("script[type='math/tex; mode=display']").each(function () {
-//         $('<p class="test">' + "$$" + $(this).text() + "$$" + "</p>").insertAfter($(this));
-//         $(this).remove();
-//     });
-// });
