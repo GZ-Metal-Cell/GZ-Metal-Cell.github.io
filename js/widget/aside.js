196120856ca0b48f46919e54bcaab2105cdf4061
GZ-Metal-Cell <1904817346@qq.com>
2024-05-07 20:38:18 +0800

    Site updated: 2024-05-07 20:38:08

--- js/widget/aside.js
+++ js/widget/aside.js
@@ -1,14 +1,14 @@
-function initAside(){
+function initAside() {
     hbeToc();
     createToc();
     activeTocItem();
     hbeShowAsideButton();
     document.addEventListener("scroll", function (event) {
         activeTocItem();
-    });    
+    });
 }
 
-function showAside(){
+function showAside() {
     var value = localStorage.getItem('aside-status');
     if (value === null) {
         localStorage.setItem('aside-status', "true");
@@ -19,7 +19,7 @@ function showAside(){
     }
 }
 
-function hbeToc(){
+function hbeToc() {
     if ($('.hbe-content').length > 0) {
         $('.post-content').addClass('hbe-toc');
     } else {
@@ -38,76 +38,66 @@ function hbeShowAsideButton() {
 }
 
 function createToc() {
-    var toc = $('.toc');
-    toc.empty();
-
+    var toc = $('<ol>').addClass('toc');
     var headings = $('.post-content').find('h1, h2, h3, h4, h5, h6');
     var currentLevel = 1;
-    var currentList = toc;
+    var currentParent = toc;
 
-    for (var i = 0; i < headings.length; i++) {
-        var heading = $(headings[i]);
-        // ID 开头不能为数字，如果为了，加个下划线
-        if (/^[0-9]/.test(heading.attr('id'))) {
-            heading.attr('id', '_' + heading.attr('id'));
+    headings.each(function(index, heading) {
+        if (/^[0-9]/.test($(heading).attr('id'))) {
+            $(heading).attr('id', '_' + $(heading).attr('id'));
         }
-        if (!heading.find('a').length)  // 标题里没有<a>，可能是用户自己创建的标题，跳过
-            continue;
-        else if (!heading.find('a').hasClass('headerlink'))  // 标题里有 <a>，但没有 headerlink 类，跳过
-            continue;
-        var level = parseInt(heading.prop('tagName').charAt(1));
-        // 创建目录
-        if (level > currentLevel) {
-            for (var j = currentLevel + 1; j <= level; j++) {
-                var newOl = $('<ol>').addClass('toc-child');
-                var newLi = $('<li>').addClass('toc-item toc-level-level1-' + j);
-                currentList.append(newLi);
-                newLi.append(newOl);
-                currentList = newOl;
-            }
-        } else if (level < currentLevel) {
-            for (var j = level; j < currentLevel; j++) {
-                currentList = currentList.parent().parent();
-            }
-        }
-        var li = $('<li>').addClass('toc-item toc-level-level-' + level);
-        // 获取 hrefValue
-        var hrefValue = heading.html().match(/href="([^"]+)"/) ? heading.html().match(/href="([^"]+)"/)[1] : '';
-        // ID 开头不能为数字，如果为了，加个下划线
-        if (!isNaN(parseInt(hrefValue.charAt(1)))) {
-            hrefValue = hrefValue.slice(0, 1) + "_" + hrefValue.slice(1);
-        }
-        // 获取 titleValue
-        var titleValue = heading.html().match(/title="([^"]+)"/) ? heading.html().match(/title="([^"]+)"/)[1] : '';
-        // 创建 <a>
-        if (level > 1) {
-            li.html('<a class="toc-link" href="' + hrefValue + '">' + '<span style="margin-left: ' + (level - 1) + 'em;" class="text">' + titleValue + '</span></a>');
-        } else
-        {
-            li.html('<a class="toc-link" href="' + hrefValue + '">' + '<span class="text">' + titleValue + '</span></a>');
-        }
-        var a = li.find("a");
-        // 重写点击目录时的跳转逻辑
+        if ((!$(heading).find('a').length) || (!$(heading).find('a').hasClass('headerlink')))  // 标题里没有<a>，可能是用户自己创建的标题，跳过
+            return;
+
+        var level = parseInt(heading.tagName.charAt(1));
+        
+        // 创建列表项和链接
+        var li = $('<li>').addClass('toc-item toc-level-' + level);
+        var a = $('<a>').addClass('toc-link').attr('href', '#' + heading.id);
+        var span = $('<span>').addClass('text').text($(heading).text());
+
+        a.append(span);
+
         a.on("click", function (event) {
             event.preventDefault();
-            var element = $($(this).attr("href"));
-            var rect = element[0].getBoundingClientRect();
+            var heading = $($(this).attr("href"));
+            var rect = heading[0].getBoundingClientRect();
             var topOffset = rect.top + window.scrollY - 70;
             window.scrollTo({
                 top: topOffset,
                 behavior: "smooth"
             });
         });
-        currentList.append(li);
+
+        li.append(a);
+
+        // 确定父级
+        if (level > currentLevel) {
+            var parentList = $('<ol>').addClass('toc-child');
+            currentParent.find('li:last').append(parentList);
+            currentParent = parentList;
+        } else if (level < currentLevel) {
+            for (var i = level; i < currentLevel; i++) {
+                currentParent = currentParent.parent().parent();
+            }
+        }
+
+        // 将列表项添加到父级
+        currentParent.append(li);
+
         currentLevel = level;
-    }
+    });
+
+    $('.toc').replaceWith(toc); // 将原来的目录替换为新的目录
 }
 
-function onShowAsideButton(){
+
+function onShowAsideButton() {
     document.querySelector('.post-content').classList.toggle('close-aside');
-    
+
     var value = localStorage.getItem('aside-status');
-    if(value === "false") {
+    if (value === "false") {
         activeTocItem();
         activeSeriesItem();
     }
@@ -131,7 +121,7 @@ function getTopHeadingId() {
     return topHeadingId;
 }
 
-function activeTocItem(){
+function activeTocItem() {
     const tocLinks = document.querySelectorAll('a.toc-link');
     const topHeadingId = getTopHeadingId();
     tocLinks.forEach(link => {
