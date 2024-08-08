7a1728d865f8c93c2fd801dfdcc1ff6f18f8764f
GZ-Metal-Cell <1904817346@qq.com>
2024-08-08 12:10:57 +0800

    Site updated: 2024-08-08 12:10:30

--- 2024/05/02/Hexo-侧边栏/test_aside.js
+++ 2024/05/02/Hexo-侧边栏/test_aside.js
@@ -344,7 +344,32 @@ function createNode_2() {
     return e_0;
 }
 
+function createNode_3() {
+    var e_0 = document.createElement("div");
+    e_0.setAttribute("class", "poem aside-card");
+    var e_1 = document.createElement("div");
+    e_1.setAttribute("class", "top");
+    var e_2 = document.createElement("h1");
+    e_2.appendChild(document.createTextNode("音乐"));
+    e_1.appendChild(e_2);
+    var e_3 = document.createElement("div");
+    const ap = new APlayer({
+        container: e_3,
+        audio: [{
+            name: '梦之津渡',
+            artist: '啃书',
+            url: '/musics/梦之津渡.mp3',
+            cover: '/musics/梦之津渡.jpg'
+        }]
+    });
+    e_0.appendChild(e_3);
+    return e_0;
+}
+
 $(document).ready(function() {
     document.querySelector(".aside-top").appendChild(createNode_1());
     document.querySelector(".aside-bottom").appendChild(createNode_2());
+    var parentElement = document.querySelector(".aside-bottom");
+    var firstChild = parentElement.firstChild;
+    parentElement.insertBefore(createNode_3(), firstChild);
 });
