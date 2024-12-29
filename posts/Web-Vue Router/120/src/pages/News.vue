1ca3b5068c926e9b5fb9a8b62fe32a8f04e32bf8
GZ-Metal-Cell <1904817346@qq.com>
2024-12-29 08:03:22 +0800

    Site updated: 2024-12-29 08:02:53

---
+++ posts/Web-Vue Router/120/src/pages/News.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <ul>
+            <li>
+                news001
+            </li>
+            <li>
+                news002
+            </li>
+            <li>
+                news003
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+/* eslint-disable vue/multi-word-component-names */
+export default {
+    name: 'News',
+}
+</script>
